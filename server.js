9b4b693dd2c6a88007d934dd1b290eec4a8ab79e
Mohana Prakash <mohan@ggexcel.ai>
2024-09-03 20:12:51 +0530

    deployment push

--- server.js
+++ server.js
@@ -8,27 +8,25 @@ const PORT = 5000;
 
 app.use(cors());
 
-const apiUrl1 = `https://www.babamurli.com/01.%20Daily%20Murli/03.%20Tamil/01.%20Tamil%20Murli%20-%20Htm/${current_date}-Tamil.htm`;
-const apiUrl2 = `https://www.babamurli.com/01.%20Daily%20Murli/02.%20English/01.%20Eng%20Murli%20-%20Htm/${current_date}-E.htm`;
-
-app.get("/fetch-murli", async (req, res) => {
+app.get("/fetch-murli/:pathValue", async (req, res) => {
   try {
-    // Make both API requests concurrently using Promise.all
+    const date = req.params.pathValue;
     const [response1, response2] = await Promise.all([
-      axios.get(apiUrl1),
-      axios.get(apiUrl2),
+      axios.get(
+        `https://www.babamurli.com/01.%20Daily%20Murli/03.%20Tamil/01.%20Tamil%20Murli%20-%20Htm/${date}-Tamil.htm`
+      ),
+      axios.get(
+        `https://www.babamurli.com/01.%20Daily%20Murli/02.%20English/01.%20Eng%20Murli%20-%20Htm/${date}-E.htm`
+      ),
     ]);
 
-    // Combine responses or format as needed
     const combinedResponse = {
       tamil: response1.data,
       english: response2.data,
     };
 
-    // Send combined response to client
     res.json(combinedResponse);
   } catch (error) {
-    console.error("Error fetching data:", error);
     res.status(500).json({ error: "Failed to fetch data" });
   }
 });
